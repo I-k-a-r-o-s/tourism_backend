6a9bdfc82b1e811f8eebbafc72662eaeb147406d
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-11 11:18:04 +0530

    small tweak to the route

--- src/routes/listingRoutes.js
+++ src/routes/listingRoutes.js
@@ -12,7 +12,7 @@ const listingRoutes = Router();
 
 listingRoutes.post("/create", protectedRoute, addListItem); //create a new listing
 listingRoutes.get("/getlisting/:id", getListingsByID); //get listing by id
-listingRoutes.get("/getalllistings", getAllListings); //get all listings
+listingRoutes.get("/getlistings", getAllListings); //get all listings
 listingRoutes.put("/update/:id", protectedRoute, updateListing); //update listing by id
 listingRoutes.delete("/delete/:id", protectedRoute, deleteListing); //delete listing by id
 
